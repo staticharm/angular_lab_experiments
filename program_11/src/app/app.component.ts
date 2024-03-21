2f12ee814be3d994efe6cc9fdb36611b4d6b679e
staticharm <141055451+staticharm@users.noreply.github.com>
2024-03-21 19:55:47 +0530

    Add files via upload

---
+++ program_11/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'program_11';
+
+  name: any = 'test name';
+
+}
